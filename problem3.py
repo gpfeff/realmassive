f314f67ea84e2759829e4d13e373a76bf67d6295
gpfeff <greg.a.pfeffer@gmail.com>
2014-02-09 00:06:37 -0600

    Python problem solves mean/median

--- problem3.py
+++ problem3.py
@@ -1,14 +1,27 @@
 data = [55, 99, 54, -38, -97, 92, -77, 17, -18, -35, 78, 27, 47, 48, -27, 85, 19, -55, -84, 16]
 #Sets variable, 'data', equal to an array of numbers#
 
+
 sum_data = sum(data)
 #Sets variable, 'sum_data', which is the sum of all the numbers in data#
 
 data_length = len(data)
 #Sets variable, 'data_length', equal to the length of the array(how many numbers are in there)#
 
-mean = sum_data / data_length
+data_mean = sum_data / data_length
 #Computes the mean by dividing the sum of all numbers in 'data' by the length of the array#
 
-print "The mean of the array is ", mean
+
+first_value = data[9]
+second_value = data[10]
+#Given data_length = 20, in order to find the median of data I'll need to find the mean of the two middle numbers#
+
+data_median = (first_value + second_value) / 2
+
+
+
+print "The median of the array is ", data_median
+print "The mean of the array is ", data_mean
+
+
 
