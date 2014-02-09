12883dc0aa4bd62c99b3899308348de7971b0b49
gpfeff <greg.a.pfeffer@gmail.com>
2014-02-08 23:43:08 -0600

    git remote add origin https://github.com/gpfeff/realmassive.git exit eend exit end quit

---
+++ problem3.py
@@ -0,0 +1,14 @@
+data = [55, 99, 54, -38, -97, 92, -77, 17, -18, -35, 78, 27, 47, 48, -27, 85, 19, -55, -84, 16]
+#Sets variable, 'data', equal to an array of numbers#
+
+sum_data = sum(data)
+#Sets variable, 'sum_data', which is the sum of all the numbers in data#
+
+data_length = len(data)
+#Sets variable, 'data_length', equal to the length of the array(how many numbers are in there)#
+
+mean = sum_data / data_length
+#Computes the mean by dividing the sum of all numbers in 'data' by the length of the array#
+
+print "The mean of the array is ", mean
+
