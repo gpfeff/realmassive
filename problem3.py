65eeab52b751aeaf5bc2345a2e69550c08dca076
gpfeff <greg.a.pfeffer@gmail.com>
2014-02-09 21:05:08 -0600

    Mean/Median/Mode all pass, with inclusion of if/else in case no mode exists

--- problem3.py
+++ problem3.py
@@ -1,4 +1,4 @@
-data = [55, 99, 54, -38, -97, 92, -77, 17, -18, -35, 78, 27, 47, 48, -27, 85, 19, -55, -84, 16]
+data = [55, 99, 54, -38, -97, 92, -77, 17, -18, -35, 78, 27, 47, 48, -27, 85, 19, -55, -84, 16,]
 #Sets variable, 'data', equal to an array of numbers#
 
 sum_data = sum(data)
@@ -10,6 +10,10 @@ data_length = len(data)
 data_mean = float(sum_data)/data_length
 #Computes the mean by dividing the sum of all numbers in 'data' by the length of the array & converts to float#
 
+print "The mean of the array is: ", data_mean
+
+
+
 data.sort()
 first_value = data[9]
 second_value = data[10]
@@ -17,19 +21,30 @@ second_value = data[10]
 
 data_median = (first_value + second_value) / 2
 
+print "The median of the array is: ", data_median
+
+
+
 from collections import Counter
 def mode(values):
   count = Counter(values)
   winner = max(count.values())
   return [k for k,v in count.items() if v == winner]
 
+ 
+mode_of_data = mode(data)
+mode_length = len(mode_of_data)
+#Given that the sample data set contains all non-repeating integers, I needed to build an if/else to catch this#
+
+
+if mode_length == data_length:
+  print "Sorry, this data contains no mode"
+  #Data_length is previously defined as the length of the array#
+else:
+   print "The mode(s) of the array:", mode(data)
 
 
 
-
-print "The mean of the array is ", data_mean
-print "The median of the array is ", data_median
-print "The mode of the array is ", mode(data)
 
 
 
